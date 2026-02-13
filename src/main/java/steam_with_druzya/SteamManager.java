ae60b6a03450f36b7f5f341dbe3271d46e0ac296
Alpha-Potat <150262579+Alpha-Potat@users.noreply.github.com>
2026-02-13 17:46:47 +0300

    Pipisya

--- src/main/java/steam_with_druzya/SteamManager.java
+++ src/main/java/steam_with_druzya/SteamManager.java
@@ -17,6 +17,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+//hahahaha
 
 public class SteamManager {
     public static final SteamManager INSTANCE = new SteamManager();
